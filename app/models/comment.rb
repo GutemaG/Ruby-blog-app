9daa49de5439b5f424a8dc9efcc3be520d09bd9e
GutemaG <bir13gud@gmail.com>
2023-03-30 22:12:39 +0300

    Unit test with rspec for all models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_post_counter
+  after_save :update_comment_counter
 
   def update_comment_counter
     post.update(comments_counter: post.comments.count)
