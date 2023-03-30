24820545baf5233206789ff1ca63e193d7941146
GutemaG <bir13gud@gmail.com>
2023-03-30 20:41:31 +0300

    Add methods to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_save :update_post_counter
+
+  def recent_five_comments
+    comments.last(5)
+  end
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
