6f93d75711fe62ab9efb21402f5480939af86c10
GutemaG <bir13gud@gmail.com>
2023-04-04 22:42:57 +0300

    Add test cases to controller test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,14 +12,24 @@ RSpec.describe PostsController, type: :request do
       expect(response).to render_template(:index)
     end
 
+    it 'Should return the correct placeholder for post' do
+      get '/users/:id/posts'
+      expect(response.body).to include('Users Post')
+    end
+
+    it 'Should response success response for post show method' do
+      get '/users/:id/posts/:id'
+      expect(response).to be_successful
+    end
+
     it 'Should render show view for user post' do
       get '/users/:id/posts/:id'
       expect(response).to render_template(:show)
     end
 
-    it 'Should return the correct placeholder for post' do
-      get '/users/:id/posts'
-      expect(response.body).to include('Users Post')
+    it 'Should return the correct placeholder for post#show' do
+      get '/users/:id/posts/:id'
+      expect(response.body).to include('Detail for post')
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,5 +16,20 @@ RSpec.describe UsersController, type: :request do
       get '/users/'
       expect(response.body).to include('Lise of users with number of their posts')
     end
+
+    it 'Should return 200 response for user#show route' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Should render show template' do
+      get '/users/:id'
+      expect(response).to render_template(:show)
+    end
+
+    it 'Should return correct placeholder text for #show view' do
+      get '/users/:id'
+      expect(response.body).to include('User Detail')
+    end
   end
 end
