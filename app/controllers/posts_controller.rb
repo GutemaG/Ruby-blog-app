61d85b0191a99a160e5cbdf5292efc3ec25c3931
GutemaG <bir13gud@gmail.com>
2023-04-13 21:29:08 +0300

    User can delete only their posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments, :author)
@@ -20,15 +22,29 @@ class PostsController < ApplicationController
     @post.likes_counter = 0
     @post.id = rand(1000)
     if @post.save
-      redirect_to user_posts_path
+      redirect_to user_posts_path, notice: 'post Created Successfully'
     else
       render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.comments.each(&:destroy)
+    @post.likes.each(&:destroy)
+
+    if @post.destroy
+      redirect_to user_path(current_user), notice: 'Post Deleted Successfully'
+    else
+      flash.now[:error] = 'Error! Post Not deleted'
+      render :show, status: :unprocessable_entity
     end
   end
 
   private
 
   def post_params
+    @user = current_user
     params.require(:post).permit(:title, :text)
   end
 end
