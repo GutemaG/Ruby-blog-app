9c4604200041f58c0dcbc05fc173e6d3aa04841c
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-14 15:44:27 -0300

    Create api endpoint for creating comments

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,3 +1,8 @@
 class Api::ApplicationController < ActionController::API
   respond_to :json
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    respond_with '{"error": "not_found"}', status: :not_found
+  end
 end
